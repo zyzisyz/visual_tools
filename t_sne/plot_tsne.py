f9845898e155dd9ab1cfe0dcf1866e6081daf92d
my <277051592@qq.com>
2020-09-30 20:51:00 +0800

    TODO : waveform to mfcc

--- t_sne/plot_tsne.py
+++ t_sne/plot_tsne.py
@@ -4,7 +4,17 @@ import argparse
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 import kaldi_io
+import scipy.io.wavfile
 
+
+def get_rand_list(num, _range):
+    rand_list = []
+    while num != 0:
+        x = np.random.randint(0, _range)
+        if x not in rand_list:
+            rand_list.append(x)
+            num -= 1
+    return rand_list
 
 def tsne_plotter(data, label, save_png, title):
     n_labels = len(set(label))
@@ -17,17 +27,39 @@ def tsne_plotter(data, label, save_png, title):
     plt.savefig(save_png)
 
 
-def sample_data(data_dir):
+def sample_data(data_dir, n_speaker, n_frame):
     # TODO
+    f_speaker = os.listdir(data_dir)
+    rand_list = get_rand_list(n_speaker, len(f_speaker))
+    chosen_speakers = [f_speaker[idx] for idx in rand_list]
+    wave_files = []
+    for speaker in chosen_speakers:
+        now_path = os.path.join(data_dir, speaker)
+        total_frame = [] 
+        for (root, dirs, files) in os.walk(now_path):
+            total_frame += [root + f for f in files]
+        rand_list = get_rand_list(n_frame, len(total_frame))
+        frame_list = [total_frame[i] for i in rand_list]
+        wave_files += frame_list
+
+    data = []
+    for name in wave_files:
+        wav = scipy.io.wavfile.read(name) #read the waveform
+        #TODO
+        frames = mfcc(wav) #turn waveform to spectrogram
+
+        frame = frames[np.randint(len(frames))] #select a frame in spectrogram
+        data.append(frame)
 
     data = np.array(data)
-    label = np.array(label)
+    #label = np.array(label)
+    label = np.repeat(range(n_speaker) , n_frame)
     return data, label
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_dir", type=str, default="dataset",
+    parser.add_argument("--data_path", type=str, default="dataset",
                         help="file of npz data.")
     parser.add_argument("--pic_saved_path", type=str, default="tsne.png", 
                         help="file of saved png.")
@@ -39,7 +71,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     print("loading data...")
-    data, label = sample_data(args.data_dir, args.n_speaker, args.n_frame)
+    data, label = sample_data(args.data_path, args.n_speaker, args.n_frame)
 
     print("ploting...")
     tsne_plotter(data, label, args.save_png, args.title)
--- t_sne/run.sh
+++ t_sne/run.sh
@@ -2,7 +2,8 @@
 
 python plot_tsne.py \
 	--data_path data \
-	--pic_titile mfcc \
+	--pic_title mfcc \
 	--pic_saved_path tsne.png \
 	--n_speaker 10 \
 	--n_frame 10
+
